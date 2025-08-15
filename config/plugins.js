3fa9d0748e96cd7c7d74a1dd606652bc33fa3f08
topher <topherjamesknoll@gmail.com>
2025-08-15 08:10:41 -0700

    Think twice. Code once.

--- config/plugins.js
+++ config/plugins.js
@@ -25,4 +25,16 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: "strapi-provider-email-resend",
+      providerOptions: {
+        apiKey: env("RESEND_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "topher@bessaapps.com",
+        defaultReplyTo: "topher@bessaapps.com",
+      },
+    },
+  },
 });
